d77983372b6210f54abc4bea2088525e748c3c3c
Jongwon Park <pjw970220@gmail.com>
2024-02-14 16:55:57 +0900

    저장된 모든 account 정보 불러오는 버그 수정 정규식을 이용해 tagLine과 gameName의 내용이 존재하지 않으면 모든 유저의 정보를 불러오는 버그가 있어 gameName을 입력하지 않으면 공백 배열 전송하게 변경

--- src/potg/riot/common/api/autocomplete/autocomplete/autocomplete.service.ts
+++ src/potg/riot/common/api/autocomplete/autocomplete/autocomplete.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Account } from "../../account/schema/account.schema";
 import { Model } from "mongoose";
+import { AccountDto } from "../../account/dto/account.dto";
 
 @Injectable()
 export class AutocompleteService {
@@ -9,21 +10,42 @@ export class AutocompleteService {
     @InjectModel(Account.name) private accountModel: Model<Account>
   ) {}
 
-  async getAutocompleteByRiotId(tagLine: string, gameName: string) {
+  async getAutocompleteByRiotId(
+    tagLine: string,
+    gameName: string
+  ): Promise<AccountDto[]> {
     // const _riotId = {
     //   gameName: riotId.split("#")[0],
     //   tagLine: riotId.split("#")[1],
     // };
     // return { tagLine: tagLine, gameName: gameName };
     console.log(tagLine, gameName);
-    return await this.accountModel
-      .find({
-        tagLine: { $regex: `^${tagLine ?? ""}`, $options: "i" },
-        gameName: { $regex: `^${gameName}`, $options: "i" },
-      })
-      .collation({ locale: "ko", strength: 2, alternate: "shifted" })
-      .then((account) => {
-        return account;
-      });
+    const test = gameName.replaceAll("", " ");
+    console.log(test);
+    if (gameName)
+      return await this.accountModel
+        .find({
+          tagLine: {
+            $regex: new RegExp(
+              `^${tagLine === undefined ? "" : tagLine.split("").join("\\s*")}`
+            ),
+            $options: "i",
+          },
+          gameName: {
+            $regex: new RegExp(`^${gameName.split("").join("\\s*")}`),
+            $options: "i",
+          },
+        })
+        .collation({
+          locale: "ko",
+          strength: 2,
+          alternate: "shifted",
+          normalization: true,
+        })
+        .exec()
+        .then((account) => {
+          return account;
+        });
+    else return [];
   }
 }
